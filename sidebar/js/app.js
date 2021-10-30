fa5061e92b21578a8cf3181ea3d66ff61b9900f5
Adil <marwatadil@yahoo.com>
2021-10-30 19:49:01 +0500

    Sidebar for pc

---
+++ sidebar/js/app.js
@@ -0,0 +1,25 @@
+function openTab(evt, TabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(TabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+function menuAnimation() {
+  var element = document.getElementById("tab");
+  var element1 = document.getElementById("admin_nav");
+  if (element && element1) {
+    element.classList.toggle("navSlide");
+    element1.classList.toggle("navSlide1");
+  }
+}
